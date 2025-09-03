18348bcdd655e9d9970b3ccd179b351a895b8849
Eduardo Aire <hola@eduairet.com>
2025-09-03 16:58:38 -0600

    Basic styles

--- accessible-app/src/App.tsx
+++ accessible-app/src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
   return (
-    <>
+    <main className="p-4 max-w-[850px] mx-auto">
       <h1>Accessible App</h1>
       <h2>Welcome to the Accessible App!</h2>
       <p>This application is designed with accessibility in mind.</p>
-    </>
+    </main>
   );
 }
 
