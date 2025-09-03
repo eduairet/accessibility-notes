a267667cfbc839d1f1a011f47e0d0291a995d498
Eduardo Aire <hola@eduairet.com>
2025-09-03 16:54:20 -0600

    App init

---
+++ accessible-app/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Accessible App</h1>
+      <h2>Welcome to the Accessible App!</h2>
+      <p>This application is designed with accessibility in mind.</p>
+    </>
+  );
+}
+
+export default App;
